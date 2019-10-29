59569177be265a07616fde8f41e704f6c3754684
Michael Nash <michael@spatialnetworks.com>
2019-10-29 12:59:41 -0400

    typescript annotations

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,26 @@ import axios from 'axios'
 
 const url = 'http://jsonplaceholder.typicode.com/todos/5'
 
+interface Todo {
+	id: number
+	title: string
+	completed: boolean
+}
+
 axios.get(url).then(response => {
-	console.log(response.data)
+	const todo: Todo = response.data
+
+	const id = todo.id
+	const title = todo.title
+	const completed = todo.completed
+
+	logTodo(id, title, completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+	console.log(`
+    The Todo with the ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `)
+}
