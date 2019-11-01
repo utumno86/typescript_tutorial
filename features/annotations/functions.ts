147a6fc94072bfeb70855329fd172304cba02ec1
Michael Nash <michael@spatialnetworks.com>
2019-11-01 15:36:10 -0400

    furtherer fun with functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -3,5 +3,21 @@ const add = (a: number, b: number): number => {
 }
 
 const subtract = (a: number, b: number) => {
-	a - b
+	return a - b
+}
+
+const divide = (a: number, b: number): number => {
+	return a / b
+}
+
+const multiply = (a: number, b: number): number => {
+	return a * b
+}
+
+const logger = (message: string): void => {
+	console.log(message)
+}
+
+const throwError = (message: string): never => {
+	throw new Error(message)
 }
