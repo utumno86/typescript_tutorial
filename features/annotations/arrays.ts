3a7febec1172ae805972a0c548e5dfed3ecb16d4
Michael Nash <michael@spatialnetworks.com>
2019-11-05 16:03:44 -0500

    fun with arrays

---
+++ features/annotations/arrays.ts
@@ -0,0 +1,25 @@
+const carMakers: string[] = []
+
+const dates = [new Date(), new Date(), new Date()]
+
+const carsByMake: string[][] = []
+
+// Help with inference when extracting values
+
+const myCar = carMakers[0]
+const myCar2 = carMakers.pop()
+
+// Prevent incompatible values
+
+carMakers.push(100)
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+	return car.toUpperCase()
+})
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date(), '2030-10-10']
+importantDates.push('2030-10-10')
+importantDates.push(new Date())
+importantDates.push(100)
