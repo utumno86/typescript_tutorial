7072d227ccd9769697292e198d670bfd2354d7b3
Michael Nash <michael@spatialnetworks.com>
2019-11-07 11:34:54 -0500

    generic interfaces

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,19 +1,27 @@
-interface Vehicle {
-	name: string
-	year: number
-	broken: boolean
+interface Reportable {
+	summary(): string
 }
 
 const oldCivic = {
 	name: 'civic',
 	year: 2000,
 	broken: true,
+	summary(): string {
+		return `Name: ${this.name}`
+	},
 }
 
-const printVehicle = (vehicle: Vehicle): void => {
-	console.log(`Name: ${vehicle.name}`)
-	console.log(`Year: ${vehicle.year}`)
-	console.log(`Broken: ${vehicle.broken}`)
+const drank = {
+	color: 'brown',
+	sugar: 40,
+	summary(): string {
+		return `Sugar: ${this.sugar}`
+	},
 }
 
-printVehicle(oldCivic)
+const printSummary = (item: Reportable): void => {
+	console.log(item.summary())
+}
+
+printSummary(oldCivic)
+printSummary(drank)
